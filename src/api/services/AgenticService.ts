49e7d75fa88d463da81d003f1572e5369677dac4
Roni Dover <roni.dover@gmail.com>
2025-10-01 11:41:52 -0700

    removing legacy functionality

--- src/api/services/AgenticService.ts
+++ src/api/services/AgenticService.ts
@@ -1,15 +1,5 @@
 import { Service } from "./Service";
-import type { GetIncidentPayload, GetIncidentResponse } from "./types";
 
 export class AgenticService extends Service {
   protected readonly basePath = "/Agentic";
-
-  public async getIncident(
-    data: GetIncidentPayload
-  ): Promise<GetIncidentResponse> {
-    const response = await this.client.client.get<GetIncidentResponse>(
-      this.getUrl(`incidents/${data.id}`)
-    );
-    return response.data;
-  }
 }
